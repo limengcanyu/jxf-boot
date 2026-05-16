3ef0e9699375393efdddff041a0ed79972129446
limengcanyu <limengcanyu@163.com>
2026-05-16 09:14:53 +0800

    update

---
+++ asura-ddd-structure/src/test/java/org/asura/ddd/structure/AsuraDddStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asura.ddd.structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsuraDddStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
