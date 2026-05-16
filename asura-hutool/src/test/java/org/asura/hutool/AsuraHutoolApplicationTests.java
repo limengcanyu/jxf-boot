95a6535f1921343152779f4f104e3f1a3cbb0a32
limengcanyu <limengcanyu@163.com>
2026-05-16 09:37:19 +0800

    update

---
+++ asura-hutool/src/test/java/org/asura/hutool/AsuraHutoolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asura.hutool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsuraHutoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
