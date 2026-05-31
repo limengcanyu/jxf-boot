b3d39874925ee3d611c2010f7a53cf3c34e8704a
limengcanyu <limengcanyu@163.com>
2026-05-31 10:18:16 +0800

    update

---
+++ asura-statemachine/src/test/java/org/asura/statemachine/AsuraStatemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asura.statemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsuraStatemachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
